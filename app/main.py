1534c801520d35e47e3f024d416e93a682c0d857
div2113 <divyajadhav2113@gmail.com>
2026-07-26 18:51:46 +0530

    feat : Implementing  Router for basic api endpoints

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import Session
 from app.database import Base ,engine , get_db
 from app import models , schemas , crud
 
+from app.routers.user import router as user_router
+
 app = FastAPI()
+app.include_router(user_router)
 
 Base.metadata.create_all(bind=engine)
 
@@ -14,74 +17,74 @@ def home():
         "message": "Welcome to AI Chatbot API"
     }
 
-@app.post("/users" , response_model=schemas.UserResponse)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    return crud.create_user(db=db , user=user)
+# @app.post("/users" , response_model=schemas.UserResponse)
+# def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+#     return crud.create_user(db=db , user=user)
 
 
-@app.get("/users", response_model=list[schemas.UserResponse])
-def get_users(db: Session = Depends(get_db)):
-    return crud.get_users(db)
+# @app.get("/users", response_model=list[schemas.UserResponse])
+# def get_users(db: Session = Depends(get_db)):
+#     return crud.get_users(db)
 
 
-@app.get("/users/{user_id}" , response_model=schemas.UserResponse)
-def get_user(user_id:int , db: Session= Depends(get_db)):
-    user =  crud.get_user(db , user_id)
+# @app.get("/users/{user_id}" , response_model=schemas.UserResponse)
+# def get_user(user_id:int , db: Session= Depends(get_db)):
+#     user =  crud.get_user(db , user_id)
 
-    if user is None:
-        raise  HTTPException(
-            status_code=404,
-            detail = "User not found"
-        )
+#     if user is None:
+#         raise  HTTPException(
+#             status_code=404,
+#             detail = "User not found"
+#         )
 
-    return user
+#     return user
 
 
-@app.put("/users/{user.id}", response_model=schemas.UserResponse)
-def update_user(
-    user_id:int ,
-    updated_user: schemas.UserUpdate ,
-    db: Session = Depends(get_db)
-):
-    user = crud.update_user(db , user_id , updated_user)
+# @app.put("/users/{user.id}", response_model=schemas.UserResponse)
+# def update_user(
+#     user_id:int ,
+#     updated_user: schemas.UserUpdate ,
+#     db: Session = Depends(get_db)
+# ):
+#     user = crud.update_user(db , user_id , updated_user)
 
-    if user is None:
-        raise HTTPException(
-            status_code = 404 ,
-            detail ="User not found"
-        )
+#     if user is None:
+#         raise HTTPException(
+#             status_code = 404 ,
+#             detail ="User not found"
+#         )
 
-    return user
+#     return user
 
-@app.patch("/users/{user_id}" , response_model=schemas.UserResponse)
-def patch_user(
-    user_id:int,
-    updated_user :schemas.UserPatch ,
-    db: Session =Depends(get_db)
-):
-    user = crud.patch_user(db,user_id , updated_user)
+# @app.patch("/users/{user_id}" , response_model=schemas.UserResponse)
+# def patch_user(
+#     user_id:int,
+#     updated_user :schemas.UserPatch ,
+#     db: Session =Depends(get_db)
+# ):
+#     user = crud.patch_user(db,user_id , updated_user)
 
-    if user is None:
-        raise HTTPException(
-            status_code=404,
-            detail ="User not found"
-        )
+#     if user is None:
+#         raise HTTPException(
+#             status_code=404,
+#             detail ="User not found"
+#         )
 
-    return user
+#     return user
 
-@app.delete("/users/{user_id}")
-def delete_user(
-    user_id:int,
-    db:Session = Depends(get_db)
-):
-    user=crud.delete_user(db,user_id)
+# @app.delete("/users/{user_id}")
+# def delete_user(
+#     user_id:int,
+#     db:Session = Depends(get_db)
+# ):
+#     user=crud.delete_user(db,user_id)
 
-    if user is None:
-        raise HTTPException(
-            status_code=404,
-            detail="User not found"
-        )
+#     if user is None:
+#         raise HTTPException(
+#             status_code=404,
+#             detail="User not found"
+#         )
 
-    return {
-        "message": "User deleated successfully"
-    }
+#     return {
+#         "message": "User deleated successfully"
+#     }
---
+++ app/routers/user.py
@@ -0,0 +1,83 @@
+from fastapi import APIRouter , Depends , HTTPException
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app import schemas , crud
+
+router = APIRouter(
+    prefix = "/users",
+    tags=["Users"]
+)
+
+@router.post("/", response_model=schemas.UserResponse)
+def create_user(
+    user : schemas.UserCreate,
+    db: Session = Depends(get_db)
+):
+    return crud.create_user(db=db , user=user)
+
+
+@router.get("/", response_model=list[schemas.UserResponse])
+def get_users(db: Session = Depends(get_db)):
+    return crud.get_users(db)
+
+@router.get("/{user_id}" , response_model=schemas.UserResponse)
+def get_user(user_id:int , db: Session= Depends(get_db)):
+    user =  crud.get_user(db , user_id)
+
+    if user is None:
+        raise  HTTPException(
+            status_code=404,
+            detail = "User not found"
+        )
+
+    return user
+
+@router.put("/{user.id}", response_model=schemas.UserResponse)
+def update_user(
+    user_id:int ,
+    updated_user: schemas.UserUpdate ,
+    db: Session = Depends(get_db)
+):
+    user = crud.update_user(db , user_id , updated_user)
+
+    if user is None:
+        raise HTTPException(
+            status_code = 404 ,
+            detail ="User not found"
+        )
+
+    return user
+
+@router.patch("/{user_id}" , response_model=schemas.UserResponse)
+def patch_user(
+    user_id:int,
+    updated_user :schemas.UserPatch ,
+    db: Session =Depends(get_db)
+):
+    user = crud.patch_user(db,user_id , updated_user)
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail ="User not found"
+        )
+
+    return user
+
+@router.delete("/users/{user_id}")
+def delete_user(
+    user_id:int,
+    db:Session = Depends(get_db)
+):
+    user=crud.delete_user(db,user_id)
+
+    if user is None:
+        raise HTTPException(
+            status_code=404,
+            detail="User not found"
+        )
+
+    return {
+        "message": "User deleated successfully"
+    }
