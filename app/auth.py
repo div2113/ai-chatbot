4c8e89710c398cbc5e873fc24808b7da008852c6
div2113 <divyajadhav2113@gmail.com>
2026-07-27 16:22:40 +0530

    Configure environment variables for authentication

--- app/auth.py
+++ app/auth.py
@@ -1,6 +1,17 @@
 from pwdlib import PasswordHash
+from jose import JWTError , jwt
+from dotenv import load_dotenv
 
+from datetime import datetime,timedelta,timezone
+import os
+
+
+load_dotenv()
 password_hash = PasswordHash.recommended()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 def hash_password(password: str):
